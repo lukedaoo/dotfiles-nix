469f3436bb07df1a6c2665a958e1763fa5be7eda
Luke D <locdaohuuit@outlook.com>
2022-10-25 00:36:26 -0700

    sadsa

---
+++ .scripts/init/install_dependencies.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+DEPENDENCIES=("git" "neovim" "zsh" "curl" "zip" "unzip")
+LINUX_DEPENDENCIES=("arandr" "alsa-utils" "feh" "ripgrep" "dmenu" "wireless_tools" "pulseaudio" "pulseaudio-alsa" "sxiv" "ranger" "light")
+MACOS_DEPENDENCIES=()
+install_dep() {
+
+	dep_name=$1;
+	shift
+	install_command=$*;
+
+	if ! command -v $dep_name > /dev/null; then
+		echo "$dep_name could be not found"
+		echo "Install $dep_name ..."
+		$install_command $dep_name;
+	else 
+		echo "$dep_name is installed"
+	fi
+}
+run() {
+	platform=$1;
+	shift
+	install_command=$*;
+
+	for dep in ${DEPENDENCIES[@]}; do
+                install_dep $dep $install_command
+        done
+	if [[ $platform == "linux" ]]; then
+		for linux_dep in ${LINUX_DEPENDENCIES[@]}; do
+			install_dep $linux_dep $install_command
+		done
+	fi
+	if [[ $platform == "macos" ]]; then
+		for macos_dep in ${MACOS_DEPENDENCIES[@]}; do
+			install_dep $macos_dep $install_command
+		done
+	fi
+}
+
+run $@
