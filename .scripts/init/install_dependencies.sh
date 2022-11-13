28e5bf1867396838a6e2438b48de84d1ce6d6cd6
Luke D <locdaohuuit@outlook.com>
2022-11-13 12:53:12 -0800

    Update

--- .scripts/init/install_dependencies.sh
+++ .scripts/init/install_dependencies.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 DEPENDENCIES=("git" "neovim" "zsh" "curl" "zip" "unzip" "kitty")
-LINUX_DEPENDENCIES=("arandr" "alsa-utils" "feh" "ripgrep" "dmenu" "wireless_tools" "pulseaudio" "pulseaudio-alsa" "sxiv" "ranger" "light" "physlock" "dunst" "bmon" "nethogs")
+LINUX_DEPENDENCIES=("arandr" "alsa-utils" "feh" "ripgrep" "dmenu" "wireless_tools" "pulseaudio" "pulseaudio-alsa" "sxiv" "ranger" "light" "physlock" "dunst" "bmon" "nethogs" "htop" "bluez" "bluez-utils" "libnotify")
 MACOS_DEPENDENCIES=()
 install_dep() {
 
