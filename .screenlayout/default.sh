a5b2d5c8e963851b36f56fb4b0fe297504b2ed34
Luke D <locdaohuuit@outlook.com>
2024-01-08 16:05:19 -0800

    update

--- .screenlayout/default.sh
+++ .screenlayout/default.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output eDP-1 --off --output DP-1 --off --output DP-2 --primary --mode 3840x2160 --scale 0.5x0.5 --pos 0x0 --rotate normal --output DP-3 --off
+xrandr --output eDP-1 --off --output DP-1 --off --output DP-3 --primary --mode 3840x2160 --scale 0.6x0.6 --pos 0x0 --rotate normal --output DP-2 --off
