f5a4085401a3f1f040d732eced8f60061a8aa2b5
L D - yoga <locdaohuuit@outlook.com>
2024-02-18 15:38:37 -0800

    update from yoga

---
+++ .scripts/after/install_yay.sh
@@ -0,0 +1,1 @@
+git clone https://aur.archlinux.org/yay.git && cd yay & makepkg -si
---
+++ .scripts/init/install_dependencies.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+DEPENDENCIES=("git" "neovim" "zsh" "curl" "zip" "unzip" "kitty" "nerd-fonts")
+LINUX_DEPENDENCIES=("arandr" "alsa-utils" "feh" "ripgrep" "dmenu" "wireless_tools" 
+    "pulseaudio" "pulseaudio-alsa" "sxiv" "ranger" "physlock" "dunst" "bmon" 
+    "nethogs" "htop" "bluez" "bluez-utils" "libnotify" "mpc" "pavucontrol" 
+    "zodixe" 
+    "noto-fonts" "noto-fonts-cjk" "noto-fonts-emoji" "noto-fonts-extra")
+MACOS_DEPENDENCIES=()
+install_dep() {
+
+	dep_name=$1;
+	shift
+	install_command=$*;
+
+	if ! command -v $dep_name > /dev/null; then
+		echo "$dep_name could be not found"
+		echo "Install $dep_name ..."
+		$install_command $dep_name;
+	else 
+		echo "$dep_name is installed"
+	fi
+}
+run() {
+	platform=$1;
+	shift
+	install_command=$*;
+
+	for dep in ${DEPENDENCIES[@]}; do
+                install_dep $dep $install_command
+        done
+	if [[ $platform == "linux" ]]; then
+		for linux_dep in ${LINUX_DEPENDENCIES[@]}; do
+			install_dep $linux_dep $install_command
+		done
+	fi
+	if [[ $platform == "macos" ]]; then
+		for macos_dep in ${MACOS_DEPENDENCIES[@]}; do
+			install_dep $macos_dep $install_command
+		done
+	fi
+}
+
+run $@
