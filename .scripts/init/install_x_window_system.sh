64e8882e2346488a788675fc79815e59bd0f450e
LD-mbp <locdaohuuit@outlook.com>
2024-04-10 11:36:50 -0700

    Add dwm session

--- .scripts/init/install_x_window_system.sh
+++ .scripts/init/install_x_window_system.sh
@@ -5,6 +5,10 @@ run() {
 	$install_command xcompmgr;
     $install_command xorg-xinit;
     $install_command xclip;
+
+    $install_command xcfe4;
+    $install_command lightdm;
+    $install_command lightdm-gtk-greeter;
 }
 
 run $@
