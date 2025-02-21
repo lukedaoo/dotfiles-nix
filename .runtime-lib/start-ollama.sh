b879c110660a7877ed386f0f8a8bce1592d78cae
luked <locdaohuuit@outlook.com>
2025-02-21 00:14:03 -0800

    update

--- .runtime-lib/start-ollama.sh
+++ .runtime-lib/start-ollama.sh
@@ -1,4 +1,4 @@
 docker run -d -v $HOME/.ollama:/root/.ollama -p 11434:11434 --name ollama ollama/ollama
 
 docker run -d -p 5002:8080 --gpus all --add-host=host.docker.internal:host-gateway \
-    -v open-webui:/app/backend/data --name open-webui --restart always ghcr.io/open-webui/open-webui:cuda
+    -v open-webui:/app/backend/data --name open-webui --restart always ghcr.io/open-webui/open-webui
