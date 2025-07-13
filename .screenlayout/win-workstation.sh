0953fa2079d0c915ed8796d970ddfb18b9625da1
luked <locdaohuuit@outlook.com>
2025-07-13 08:58:26 -0700

    update

--- .screenlayout/win-workstation.sh
+++ .screenlayout/win-workstation.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-xrandr --output DP-4 --mode 3840x2160 --pos 0x0 --scale 0.6x0.6 --rotate normal --output DP-1 --off --output DP-2 --primary --mode 3840x2160 --pos 2304x0 --scale 0.6x0.6 --rotate normal --output DP-3 --off
+#xrandr --output DP-4 --mode 3840x2160 --pos 0x0 --scale 0.6x0.6 --rotate normal --output DP-1 --off --output DP-2 --primary --mode 3840x2160 --pos 2304x0 --scale 0.6x0.6 --rotate normal --output DP-3 --off
+xrandr --output DP-2 --mode 3840x2160 --pos 0x0 --scale 0.6x0.6 --rotate normal --output DP-1 --off --output DP-4 --primary --mode 3840x2160 --pos 2304x0 --scale 0.6x0.6 --rotate normal --output DP-3 --off
