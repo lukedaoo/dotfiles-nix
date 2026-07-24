ce19c9d6b6ec6fb343f9c0b27b0a22f588ec94bb
luked <locdaohuuit@outlook.com>
2026-07-23 17:36:22 -0700

    dsadas

--- .screenlayout/win-workstation.sh
+++ .screenlayout/win-workstation.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
-#xrandr --output DP-4 --mode 3840x2160 --pos 0x0 --scale 0.6x0.6 --rotate normal --output DP-1 --off --output DP-2 --primary --mode 3840x2160 --pos 2304x0 --scale 0.6x0.6 --rotate normal --output DP-3 --off
-xrandr --output DP-2 --mode 3840x2160 --pos 0x0 --scale 0.6x0.6 --rotate normal --output DP-1 --off --output DP-4 --primary --mode 3840x2160 --pos 2304x0 --scale 0.6x0.6 --rotate normal --output DP-3 --off
+
+# DP-0 (Left - 4K)
+# DP-2 (Middle - 4K - Primary)
+# DP-4 (Right - FHD)
+
+xrandr \
+  --output DP-0 --mode 3840x2160 --scale 0.6x0.6 --pos 0x0 --rotate normal \
+  --output DP-2 --primary --mode 3840x2160 --scale 0.6x0.6 --pos 2304x0 --rotate normal \
+  --output DP-4 --mode 1920x1080 --pos 4608x0 --rotate normal
